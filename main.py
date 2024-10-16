eb633d3a6eea5e03f3b8c257fbe571fbed10d19b
Mysellff Self <nickminhsava@gmail.com>
2024-10-15 18:41:48 -0700

    init commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import flask
+from flask import request, render_template, session
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+  app.run(host='localhost', port=8080)
+  import os
+  host=os.environ.get("SERVER_HOST", "localhost")
+  try:
+    port = int(os.environ.get("SERVER_PORT", "5555"))
+  except:
+     port = 5555
+    
+  app.run(host, port, debug=True)
